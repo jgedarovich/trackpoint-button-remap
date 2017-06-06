e0dbe45bc5c83f44434c221bce4d7904c44c4e5a
jgedarovich <jgedarovich@etsy.com>
2017-06-06 21:43:42 +0000

    add more to setup

--- default.nix
+++ default.nix
@@ -5,10 +5,21 @@ stdenv.mkDerivation {
 
   src = fetchgit {
     url= "https://github.com/jgedarovich/trackpoint-button-remap.git";
-    rev = "bf93dfa1178fecb2600b4cd86b0faf4c1a1dbb49";
+    rev = "c68f5bf60683f0365625f3e3ef351e0ef4ef4515";
   };
 
-  buildInputs = [ python3 python35Packages.evdev python35Packages.python-uinput ];
+  buildInputs = [ 
+    libevdev
+    # With Python configuration requiring a special wrapper
+    (python35.buildEnv.override {
+      ignoreCollisions = true;
+      extraLibs = with python35Packages; [
+        python
+        evdev
+        python-uinput
+      ];
+    })
+  ];
 
   installPhase = ''
     bash ./setup.sh $out
--- setup.sh
+++ setup.sh
@@ -1,18 +1,23 @@
 #!/bin/sh
 
-
 PREFIX=${1:-/usr/local}
 
 echo Okay, time to install this puppy.
 set -x
-mkdir -p $PREFIX/bin || (mkdir $PREFIX; mkdir $PREFIX/bin)
-#sudo cp *.rules /etc/udev/rules.d/
-#sudo cp *.conf /etc/modules-load.d/
-#sudo udevadm trigger
+
+mkdir -p $PREFIX/bin
 cp remap.py  $PREFIX/bin/remap.py
+chmod a+x $PREFIX/bin/remap.py
+
+mkdir -p $PREFIX/etc/modules-load.d/
+cp *.conf $PREFIX/etc/modules-load.d/
+
+mkdir -p $PREFIX/etc/udev/rules.d/
+cp *.rules $PREFIX/etc/udev/rules.d/
+
+udevadm trigger
 #sudo cp trackpoint-remap.service /etc/systemd/system
 #sudo systemctl daemon-reload
 #sudo systemctl enable trackpoint-remap.service
 #sudo systemctl start trackpoint-remap.service
-chmod a+x $PREFIX/bin/remap.py
 
